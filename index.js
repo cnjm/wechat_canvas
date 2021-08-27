4b5597e20b1a761ed2742e3ffd349b4deae1fe95
cnjm <1837847333@qq.com>
2021-08-27 18:37:12 +0800

    单行文字，图片，自适应图片，默认设置，局部设置

--- index.js
+++ index.js
@@ -4,18 +4,28 @@
  */
 export default class WxCanvas {
   constructor(options) {
-    const { el, design, width, height } = options;
+    const {
+      el,
+      design,
+      width,
+      height,
+      useDpr = false
+    } = options;
     const dpr = wx.getSystemInfoSync().pixelRatio;
-    this.dpr = dpr;
+    this.dpr = useDpr ? dpr : 1;
     this.el = el;
-    this.design = design;
+    this.design = design || 750;
     this.width = width;
     this.height = height;
   }
-  static getByteLen() {
+  /**
+   * 返回一段字符串的字节数 中文2  英文1
+   * @param {需要计算的字符串} str 
+   */
+  static getByteLen(str) {
     let len = 0;
-    for (let i = 0; i < val.length; i++) {
-      let a = val.charAt(i);
+    for (let i = 0; i < str.length; i++) {
+      let a = str.charAt(i);
       if (a.match(/[^\x00-\xff]/ig) != null) { //\x00-\xff→GBK双字节编码范围
         len += 2;
       } else {
@@ -24,7 +34,11 @@ export default class WxCanvas {
     }
     return len;
   }
-  //返回换行之后的文本数组
+  /**
+   * 返回换行之后的文本数组
+   * @param {需要换行的字符串} str 
+   * @param {一行字节个数} num 
+   */
   static getBreak(str, num = 1) {
     let arr = [];
     let item = '';
@@ -37,6 +51,9 @@ export default class WxCanvas {
 
     return arr;
   }
+  /**
+   * 初始化 画布的准备将在次完成
+   */
   init() {
     return new Promise((resolve, reject) => {
       const query = wx.createSelectorQuery();
@@ -44,46 +61,27 @@ export default class WxCanvas {
         node: true,
         size: true
       }).exec((res) => {
-        res[0].width = 375;
-        res[0].height = 750;
         this.canvas = res[0].node;
-        // console.log(this.size(this.height))
-        // this.canvas.width = 750;
-        // this.canvas.height = 1800;
-        console.log(res[0])
-        // this.canvas.width = this.size(this.width);
-        // this.canvas.height = this.size(this.height);
+        this.canvas.width = res[0].width;
+        this.canvas.height = res[0].height;
         this.ctx = this.canvas.getContext('2d');
         resolve(this);
       })
     })
   }
+  /**
+   * 获取转换后给画布的 数值
+   * @param {设计稿数值} num 
+   */
   size(num) {
     return (375 * this.dpr * num) / this.design;
   }
-  // 获取当前设置之后占用了多少的高
+  /**
+   * 获取当前设置之后占用了多少的高
+   * @param {ctx所有支持的配置项} options 
+   */
   getLineHeight() {
 
-  }
-  getFilePath() {
-    // wx.canvasToTempFilePath({
-    //   x: 0,
-    //   y: 0,
-    //   canvas: wcvs.canvas, //现在的写法
-    //   quality: 1,
-    //   success: (res) => {
-    //     wx.saveImageToPhotosAlbum({
-    //       filePath: res.tempFilePath,
-    //       success: () => {
-    //         wx.showToast({
-    //           title: '保存成功',
-    //           icon: 'success',
-    //           duration: 2000
-    //         })
-    //       },
-    //     });
-    //   },
-    // });
   }
   /**
    * {font : 'bold 100px serif',fillStyle : '#d2665f'} 这将支持更好的个性化
@@ -98,6 +96,10 @@ export default class WxCanvas {
     })
     return this;
   }
+  /**
+   * @param {ctx所有支持的配置项} options 
+   * @param {ctx画布操作的回调函数} callback 
+   */
   setTempOptions(options, callback) {
     let tempObj = {}
     Object.keys(options).forEach(item => {
@@ -108,21 +110,64 @@ export default class WxCanvas {
     this.setOptions(tempObj);
     return this;
   }
-  createImage(options) {
-
-    console.log(this)
+  getImageInfo(url, w, h) {
+    return new Promise((resolve, reject) => {
+      wx.getImageInfo({
+        src: url,
+        success: function (res) {
+          const r = res.width / res.height;
+          let _w = 0,
+            _h = 0;
+          if (h) { //定高
+            _h = h;
+            _w = _h * r;
+          } else {
+            _w = w;
+            _h = _w / r;
+          }
+          res._w = _w;
+          res._h = _h;
+          resolve(res);
+          return this;
+        }
+      })
+    })
+  }
+  async getImage(path) {
+    return new Promise((resolve, reject) => {
+      const drImg = this.canvas.createImage();
+      drImg.src = path;
+      drImg.onload = () => {
+        resolve(drImg);
+        return this;
+      }
+    })
+  }
+  async drawImage(url, x, y, w, h) {
+    const info = await this.getImageInfo(url, w, h);
+    const drImg = await this.getImage(info.path);
+    this.ctx.drawImage(drImg, this.size(x), this.size(y), this.size(w), this.size(h));
+    return this;
+  }
+  async drawAutoImage(info, x, y) {
+    const drImg = await this.getImage(info.path);
+    this.ctx.drawImage(drImg, this.size(x), this.size(y), this.size(info._w), this.size(info._h));
+    return this;
   }
   fillText(text, x, y, options = {}) {
     return this.setTempOptions(options, () => {
-      this.ctx.fillText(text, x, y);
+      this.ctx.fillText(text, this.size(x), this.size(y));
     });
   }
   fillLineText() {
-    // console.log(this.getBreak())
+    return this;
+    return this.setTempOptions(options, () => {
+      this.ctx.fillText(text, this.size(x), this.size(y));
+    });
   }
-  fillRect(x, y, width, height) {
-    this.ctx.fillRect(this.size(x), this.size(y), this.size(width), this.size(height));
+  fillRect(x, y, width, height, options) {
+    return this.setTempOptions(options, () => {
+      this.ctx.fillRect(this.size(x), this.size(y), this.size(width), this.size(height));
+    });
   }
-
-
 }
